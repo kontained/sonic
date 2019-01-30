93fefec25da26b03e6103960cf064e3921b0c11b
unknown <kontained@gmail.com>
2019-01-30 12:18:53 -0600

    Implemented autofac as the IoC container.

---
+++ Sonic/Sonic.Business/BusinessAutofacModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Sonic.Business.Extended.Orders;
+using Sonic.Business.Extended.Orders.Processor;
+using Sonic.Business.Extended.Orders.Processor.Taxes;
+using Sonic.Business.Extended.Orders.Processor.Strategy;
+using Sonic.Business.Extended.Orders.Processor.Strategy.Factory;
+
+namespace Sonic.Business
+{
+    public class BusinessAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<OrderService>().As<IOrderService>();
+            builder.RegisterType<OrderProcessor>().As<IOrderProcessor>();
+            builder.RegisterType<TaxRateService>().As<ITaxRateService>();
+            builder.RegisterType<MaterialOrderItemProcessingStrategy>().Named<IOrderItemProcessingStrategy>("material");
+            builder.RegisterType<ServiceOrderItemProcessingStrategy>().Named<IOrderItemProcessingStrategy>("service");
+            builder.Register<IOrderItemStrategyFactory>(x => 
+            new OrderItemStrategyFactory(x.ResolveNamed<IOrderItemProcessingStrategy>("material"), x.ResolveNamed<IOrderItemProcessingStrategy>("service")));
+        }
+    }
+}
--- Sonic/Sonic/Program.cs
+++ Sonic/Sonic/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using Sonic.Business;
+using Sonic.Business.Extended.Orders;
 using Sonic.DTO.Basic.Items;
+using Sonic.DTO.Extended.Orders;
+using Sonic.DTO.Extended.Orders.Items;
+using Autofac;
 
 namespace Sonic
 {
@@ -7,7 +13,39 @@ namespace Sonic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var container = BuildContainer();
+
+            var orderService = container.Resolve<IOrderService>();
+
+            var order = BuildOrder();
+
+            var orderTotal = orderService.GetOrderTotal(order);
+
+            Console.WriteLine(orderTotal.ToString());
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterModule(new BusinessAutofacModule());
+
+            return containerBuilder.Build();
+        }
+
+        private static Order BuildOrder()
+        {
+            return new Order(BuildOrderItems());
+        }
+
+        private static IEnumerable<OrderItem> BuildOrderItems()
+        {
+            return new List<OrderItem>
+            {
+                new OrderItem(new Item(1, "Cheese Burger", 5.5f), 1, OrderItemType.Material),
+                new OrderItem(new Item(1, "Cherry Limeade", 2.2f), 1, OrderItemType.Material),
+                new OrderItem(new Item(1, "Delivery Service", 1.23f), 1, OrderItemType.Service),
+            };
         }
     }
 }
